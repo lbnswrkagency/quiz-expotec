2f8045804366f79fc7a44acb01e62343013216df
Zafer Güney <zaferguney@192.168.1.150>
2023-06-15 13:36:07 +0300

    hello world root domain added

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import AdminDashboard from "./Components/AdminDashboard/AdminDashboard";
 import QuizGame from "./Components/QuizGame/QuizGame";
+import HelloWorld from "./Components/HelloWorld/HelloWorld";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/" element={<HelloWorld />} />
           <Route path="/admin-dashboard" element={<AdminDashboard />} />
           <Route path="/quiz/:uniqueLink" element={<QuizGame />} />
           {/* Add more routes as needed */}
