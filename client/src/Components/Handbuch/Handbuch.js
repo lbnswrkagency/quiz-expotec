a02768a672b2caab8ed30cfa3460d34fb108bf7c
Zafer <zafer.gueney@gmail.com>
2024-10-02 11:12:36 +0300

    x

--- client/src/Components/Handbuch/Handbuch.js
+++ client/src/Components/Handbuch/Handbuch.js
@@ -12,8 +12,8 @@ const Handbuch = ({ handleHandbuchClose }) => {
       <ol className="handbuch-text">
         <li>Loggen Sie sich im Admin-Dashboard ein.</li>
         <li>Klicken Sie auf die Schaltfläche "Quiz erstellen".</li>
-        <li>Geben Sie einen Titel und eine Beschreibung für Ihr Quiz ein.</li>
-        <li>Klicken Sie auf "Erstellen", um das Quiz zu speichern.</li>
+        <li>Geben Sie einen Titel für Ihr Quiz ein.</li>
+        <li>Klicken Sie auf "Speichern", um das Quiz zu erstellen.</li>
       </ol>
 
       <h3 className="handbuch-section-title">Quiz bearbeiten</h3>
@@ -24,7 +24,7 @@ const Handbuch = ({ handleHandbuchClose }) => {
         </li>
         <li>Klicken Sie auf die Schaltfläche "Quiz bearbeiten".</li>
         <li>Nehmen Sie die gewünschten Änderungen vor.</li>
-        <li>Klicken Sie auf "Änderungen speichern".</li>
+        <li>Klicken Sie auf "Speichern", um die Änderungen zu übernehmen.</li>
       </ol>
 
       <h3 className="handbuch-section-title">Quiz löschen</h3>
@@ -33,16 +33,15 @@ const Handbuch = ({ handleHandbuchClose }) => {
           Wählen Sie im Admin-Dashboard das Quiz aus, das Sie löschen möchten.
         </li>
         <li>Klicken Sie auf die Schaltfläche "Quiz löschen".</li>
-        <li>Bestätigen Sie die Löschung.</li>
+        <li>Bestätigen Sie die Löschung im Bestätigungsdialog.</li>
       </ol>
 
       <h3 className="handbuch-section-title">Ein Quiz "live" schalten</h3>
       <p className="handbuch-text">
-        Ein Quiz wird automatisch "live" geschaltet, sobald Sie es erstellt
-        haben und es mindestens eine Frage mit mindestens einer als korrekt
-        markierten Antwort gibt. Benutzer können das Quiz sehen und daran
-        teilnehmen. Wenn Sie Änderungen an einem Quiz vornehmen, werden diese
-        sofort wirksam und sind für die Benutzer sichtbar.
+        Ein Quiz wird automatisch "live" geschaltet, sobald es mindestens eine
+        Frage mit mindestens einer als korrekt markierten Antwort enthält.
+        Benutzer können das Quiz sehen und daran teilnehmen. Änderungen an einem
+        Quiz werden sofort wirksam und sind für die Benutzer sichtbar.
       </p>
 
       <h3 className="handbuch-section-title">Frage erstellen</h3>
@@ -51,12 +50,9 @@ const Handbuch = ({ handleHandbuchClose }) => {
           Wählen Sie im Admin-Dashboard das Quiz aus, für das Sie eine Frage
           erstellen möchten.
         </li>
-        <li>Klicken Sie auf die Schaltfläche "Frage erstellen".</li>
-        <li>
-          Geben Sie den Text und die Antwortoptionen für die Frage ein.
-          Markieren Sie die richtige Antwort.
-        </li>
-        <li>Klicken Sie auf "Erstellen", um die Frage zu speichern.</li>
+        <li>Klicken Sie auf die Schaltfläche "Frage hinzufügen".</li>
+        <li>Geben Sie den Text der Frage ein.</li>
+        <li>Klicken Sie auf "Speichern", um die Frage zu erstellen.</li>
       </ol>
 
       <h3 className="handbuch-section-title">Frage bearbeiten</h3>
@@ -66,11 +62,8 @@ const Handbuch = ({ handleHandbuchClose }) => {
           möchten.
         </li>
         <li>Klicken Sie auf die Schaltfläche "Frage bearbeiten".</li>
-        <li>
-          Nehmen Sie die gewünschten Änderungen vor, einschließlich der
-          Markierung der richtigen Antwort.
-        </li>
-        <li>Klicken Sie auf "Änderungen speichern".</li>
+        <li>Nehmen Sie die gewünschten Änderungen vor.</li>
+        <li>Klicken Sie auf "Speichern", um die Änderungen zu übernehmen.</li>
       </ol>
 
       <h3 className="handbuch-section-title">Frage löschen</h3>
@@ -79,7 +72,7 @@ const Handbuch = ({ handleHandbuchClose }) => {
           Wählen Sie im Admin-Dashboard die Frage aus, die Sie löschen möchten.
         </li>
         <li>Klicken Sie auf die Schaltfläche "Frage löschen".</li>
-        <li>Bestätigen Sie die Löschung.</li>
+        <li>Bestätigen Sie die Löschung im Bestätigungsdialog.</li>
       </ol>
 
       <h3 className="handbuch-section-title">Antwort erstellen</h3>
@@ -88,13 +81,13 @@ const Handbuch = ({ handleHandbuchClose }) => {
           Wählen Sie im Admin-Dashboard die Frage aus, für die Sie eine Antwort
           erstellen möchten.
         </li>
-        <li>Klicken Sie auf die Schaltfläche "Antwort erstellen".</li>
+        <li>Klicken Sie auf die Schaltfläche "Antwort hinzufügen".</li>
         <li>Geben Sie den Text der Antwort ein.</li>
         <li>
-          Um die Antwort als die richtige Antwort zu markieren, klicken Sie auf
-          das Kontrollkästchen "Diese Antwort ist korrekt".
+          Um die Antwort als korrekt zu markieren, aktivieren Sie das
+          Kontrollkästchen "Diese Antwort ist korrekt".
         </li>
-        <li>Klicken Sie auf "Erstellen", um die Antwort zu speichern.</li>
+        <li>Klicken Sie auf "Speichern", um die Antwort zu erstellen.</li>
       </ol>
 
       <h3 className="handbuch-section-title">Antwort bearbeiten</h3>
@@ -104,14 +97,13 @@ const Handbuch = ({ handleHandbuchClose }) => {
           möchten.
         </li>
         <li>Klicken Sie auf die Schaltfläche "Antwort bearbeiten".</li>
-        <li>Nehmen Sie die gewünschten Änderungen am Text der Antwort vor.</li>
+        <li>Nehmen Sie die gewünschten Änderungen vor.</li>
         <li>
-          Um die Antwort als die richtige Antwort zu markieren, klicken Sie auf
-          das Kontrollkästchen "Diese Antwort ist korrekt". Sie können die
-          Markierung auch entfernen, indem Sie das Kontrollkästchen
-          deaktivieren.
+          Sie können die Antwort als korrekt markieren oder die Markierung
+          entfernen, indem Sie das Kontrollkästchen "Diese Antwort ist korrekt"
+          entsprechend anpassen.
         </li>
-        <li>Klicken Sie auf "Änderungen speichern".</li>
+        <li>Klicken Sie auf "Speichern", um die Änderungen zu übernehmen.</li>
       </ol>
 
       <h3 className="handbuch-section-title">Antwort löschen</h3>
@@ -121,46 +113,102 @@ const Handbuch = ({ handleHandbuchClose }) => {
           möchten.
         </li>
         <li>Klicken Sie auf die Schaltfläche "Antwort löschen".</li>
-        <li>Bestätigen Sie die Löschung.</li>
+        <li>Bestätigen Sie die Löschung im Bestätigungsdialog.</li>
       </ol>
 
-      <h3 className="handbuch-section-title">
-        Eine Farbe für das Quiz festlegen
-      </h3>
+      <h3 className="handbuch-section-title">Farbe für das Quiz festlegen</h3>
       <ol className="handbuch-text">
         <li>
           Wählen Sie im Admin-Dashboard das Quiz aus, für das Sie eine Farbe
           festlegen möchten.
         </li>
         <li>
-          Wählen Sie eine Hauptfarbe aus dem Farbwähler aus. Diese Farbe wird
-          als Hauptfarbe für das Quiz verwendet und beeinflusst die Farbgebung
-          des gesamten Quiz, einschließlich der Hintergrundfarben und
-          Schriftfarben.
+          Klicken Sie auf den Farbwähler und wählen Sie die gewünschte
+          Hauptfarbe aus.
         </li>
-        <li>Klicken Sie auf "Änderungen speichern".</li>
+        <li>
+          Die Farbeinstellungen passen sich automatisch an die gewählte
+          Hauptfarbe an und sorgen für optimale Lesbarkeit.
+        </li>
       </ol>
-      <p className="handbuch-text">
-        Die Farbeinstellungen in dieser App sind dynamisch und passen sich
-        automatisch an die von Ihnen gewählte Hauptfarbe an. Dabei wird auch die
-        Lesbarkeit der Texte sichergestellt.
-      </p>
 
-      <h3 className="handbuch-section-title">Quiz Datenanalyse</h3>
+      <h3 className="handbuch-section-title">Quiz-Datenanalyse</h3>
       <ol className="handbuch-text">
         <li>
           Wählen Sie im Admin-Dashboard das Quiz aus, dessen Daten Sie
           analysieren möchten.
         </li>
-        <li>Klicken Sie auf die Schaltfläche "Quiz Datenanalyse".</li>
+        <li>Klicken Sie auf die Schaltfläche "Quiz-Datenanalyse".</li>
         <li>
-          Sie können nun verschiedene statistische Daten zum Quiz einsehen, wie
-          zum Beispiel die Anzahl der Teilnahmen, die durchschnittliche
-          Punktzahl, die Anzahl der richtigen und falschen Antworten pro Frage
-          und vieles mehr. Diese Daten werden automatisch erfasst und
-          aktualisiert.
+          Sie erhalten Einblicke in statistische Daten wie die Anzahl der
+          Teilnehmer, durchschnittliche Punktzahlen und Antwortverhalten.
         </li>
       </ol>
+
+      <h3 className="handbuch-section-title">Hintergrundbild hochladen</h3>
+      <ol className="handbuch-text">
+        <li>
+          Wählen Sie im Admin-Dashboard das Quiz aus, dem Sie ein
+          Hintergrundbild hinzufügen möchten.
+        </li>
+        <li>
+          Klicken Sie auf die Schaltfläche "Hintergrundbild hochladen" oder
+          "Bild ersetzen", wenn bereits ein Bild vorhanden ist.
+        </li>
+        <li>
+          Wählen Sie eine Bilddatei im JPEG- oder PNG-Format von Ihrem Computer
+          aus. Achten Sie darauf, dass die Datei nicht größer als{" "}
+          <strong>2 MB</strong> ist und eine empfohlene Auflösung von{" "}
+          <strong>1920x1080 Pixel</strong> hat.
+        </li>
+        <li>Klicken Sie auf "Hochladen", um das Bild zu speichern.</li>
+        <li>
+          Das Hintergrundbild wird nun im Quiz angezeigt und verbessert die
+          visuelle Darstellung.
+        </li>
+      </ol>
+
+      <h3 className="handbuch-section-title">Kampagnen-Logo hochladen</h3>
+      <ol className="handbuch-text">
+        <li>
+          Wählen Sie im Admin-Dashboard das Quiz aus, dem Sie ein Kampagnen-Logo
+          hinzufügen möchten.
+        </li>
+        <li>
+          Klicken Sie auf die Schaltfläche "Kampagnen-Logo hochladen" oder "Logo
+          ersetzen", wenn bereits ein Logo vorhanden ist.
+        </li>
+        <li>
+          Wählen Sie eine Bilddatei im JPEG- oder PNG-Format von Ihrem Computer
+          aus. Achten Sie darauf, dass die Datei nicht größer als{" "}
+          <strong>2 MB</strong> ist und eine empfohlene Größe von{" "}
+          <strong>maximal 500x500 Pixel</strong> hat.
+        </li>
+        <li>Klicken Sie auf "Hochladen", um das Logo zu speichern.</li>
+      </ol>
+
+      <h3 className="handbuch-section-title">
+        Position des Kampagnen-Logos festlegen
+      </h3>
+      <ol className="handbuch-text">
+        <li>
+          Nachdem Sie ein Kampagnen-Logo hochgeladen haben, erscheinen
+          Auswahlfelder für die Positionierung.
+        </li>
+        <li>
+          Wählen Sie die gewünschte Position des Logos auf dem Bildschirm, z.B.
+          "Oben-Mitte", "Unten-Rechts" etc.
+        </li>
+        <li>
+          Die Position des Logos wird automatisch gespeichert und im Quiz
+          entsprechend angezeigt.
+        </li>
+      </ol>
+
+      <p className="handbuch-text">
+        Hinweis: Die Logos und Hintergrundbilder werden automatisch skaliert, um
+        auf verschiedenen Geräten optimal dargestellt zu werden.
+      </p>
 
       <button className="handbuch-close-button" onClick={handleHandbuchClose}>
         Schließen
